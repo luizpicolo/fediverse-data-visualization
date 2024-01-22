b06d583ecdc1457e75bfa251b54d7e260f28079b
Helder45 <helder.pereira@estudante.ifms.edu.br>
2024-01-22 15:56:08 -0400

    Filter done with required words.

--- pipeline/index.js
+++ pipeline/index.js
@@ -11,6 +11,6 @@ const _filter = new Filter();
 _filter.input('./import/output/combined.json')
 _filter.removeAttributes = [
   "countryName", "connections", "statuses", "countryCode", "platform", "checked_at", "thumbnail_proxy",
-  "open_registration", "description", "banner_url", "monthly_active_users", "thumbnail", "info", "https_score", "admin"
+  "open_registration", "description", "banner_url", "monthly_active_users", "thumbnail", "info", "https_score", "admin", "fileName", "location", "country", "url", "version", "last_seen_at", "first_seen_at", "peer_count", "discovered_at\r", "state", "dead", "note_count", "monthly_active_users", "added_at", "updated_at", "uptime", "up", "dead", "ipv6", "https_rank", "obs_score", "obs_rank", "open_registrations", "active_users", "email", "id"
 ];
 _filter.output('./filter/output');
